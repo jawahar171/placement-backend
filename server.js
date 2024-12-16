ee997f344fb6da70b9dc33f54236035c38a66964
jawahar171 <jawaharsha171@gmail.com>
2024-12-16 14:17:35 +0530

    done

--- server.js
+++ server.js
@@ -28,3 +28,19 @@ mongoose
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
+const http = require("http");
+const { Server } = require("socket.io");
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.on("disconnect", () => console.log("User disconnected"));
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
